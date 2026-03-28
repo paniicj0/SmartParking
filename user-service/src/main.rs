872d20035872852a7db0e23c966b8646fa6aaff0
Jovana Panic <panic.sv64.2022@uns.ac.rs>
2026-03-28 12:30:32 +0100

    Added Docker and dump files for recovering db

--- user-service/src/main.rs
+++ user-service/src/main.rs
@@ -46,12 +46,11 @@ async fn main() {
 
     let app = create_routes(state).layer(cors);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let listener = tokio::net::TcpListener::bind(addr)
         .await
         .expect("Ne mogu da bindujem port");
 
-    println!("Server pokrenut na http://127.0.0.1:3000");
 
     axum::serve(listener, app)
         .await
